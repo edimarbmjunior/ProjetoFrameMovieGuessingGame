ae1e2cddab1e86453523826a22f66b1853fed4d6
Edimar <edimarbmjunior@gmail.com>
2026-04-28 00:40:17 -0300

    commit versao 0.0.1 - com identacao do arquivo correta

--- app/src/main/java/com/project/movie/frame/projetoframemovieguessinggame/ui/theme/Theme.kt
+++ app/src/main/java/com/project/movie/frame/projetoframemovieguessinggame/ui/theme/Theme.kt
@@ -1,6 +1,5 @@
 package com.project.movie.frame.projetoframemovieguessinggame.ui.theme
 
-import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
